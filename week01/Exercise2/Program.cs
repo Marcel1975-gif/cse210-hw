7576b2e54eb9cf710e44ba0fdb6dd24e36b2a5cc
Marcelo1975-gif <marcelocandidodasilva1975@gmail.com>
2025-08-31 15:46:53 -0300

    exe02 finished

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
+        Console.Write("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+        
+
+        if (percentage >= 90)
+        {
+            Console.WriteLine("Your letter grade is A");
+        }
+        else if (percentage >= 80)
+        {
+            Console.WriteLine("Your letter grade is B");
+        }
+        else if (percentage >= 70)
+        {
+            Console.WriteLine("Your letter grade is C");
+        }
+        else if (percentage >= 60)
+        {
+            Console.WriteLine("Your letter grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your letter grade is F");
+        }
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time.");
+        }
+
+
+        
 }
