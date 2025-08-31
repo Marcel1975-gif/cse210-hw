2f73d6ba68e4d889ed5a7cffe59c979a982dc243
Marcelo1975-gif <marcelocandidodasilva1975@gmail.com>
2025-08-31 17:48:35 -0300

    exe03 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,10 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 10; i++)
+        // part 1 ,2 
+     /* Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("lower");
+
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        */
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+        int count = 0;
+        while (guess != number)
         {
-            Console.WriteLine(i);
+            if (guess < number)
+            {
+                Console.WriteLine("higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("lower");
+
+            }
+            count++;
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
         }
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        count++;
+        Console.WriteLine($"You guessed it! It took you {count} tries.");
     }
 }
