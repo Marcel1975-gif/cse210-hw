4309bd2d6617613415a463ee4a8e2d74fe9d4e02
Marcelo1975-gif <marcelocandidodasilva1975@gmail.com>
2025-09-01 18:34:02 -0300

    changed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,24 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
-    }
-}
+        int item;
+        List<int> numbers = new List<int>();
+
+
+
+        while (item != 0)
+        {
+            Console.Write("Enter a number or type [0] to scape: ");
+            item = int.Parse(Console.ReadLine());
+            if (item != 0)
+            {
+                numbers.Add(item);
+            }
+            else
+            {
+                break;
+            }         
+        }
