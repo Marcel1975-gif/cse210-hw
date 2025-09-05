ffc9bcad0c46323a9d49cc7afa01d1c4d0fbf399
Marcelo1975-gif <marcelocandidodasilva1975@gmail.com>
2025-09-04 21:08:56 -0300

    changed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -12,18 +12,23 @@ class Program
         do
         {
 
-            Console.Write("Enter a number or type [0] to scape: ");
+            Console.Write("Enter a list of number or type [0] to finish: ");
             item = int.Parse(Console.ReadLine());
 
 
             numbers.Add(item);
 
         } while (item != 0);
-        for(int i = 0; i < numbers.Count; i++)
+        for (int i = 0; i < numbers.Count; i++)
         {
             Console.WriteLine(numbers[i]);
         }
-        
+        int cont = 0;
+        foreach (int number in numbers)
+        {
+            cont = cont + number;
+        }
+        Console.WriteLine($"The sum is: {cont}");
     }
 
 }
