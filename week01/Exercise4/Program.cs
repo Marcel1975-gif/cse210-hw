f05185dadd1aa7f5139e4704638853d304524af0
Marcelo1975-gif <marcelocandidodasilva1975@gmail.com>
2025-09-04 21:19:07 -0300

    changed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -7,8 +7,6 @@ class Program
         int item = 0;
         List<int> numbers = new List<int>();
 
-
-
         do
         {
 
@@ -23,15 +21,36 @@ class Program
         {
             Console.WriteLine(numbers[i]);
         }
+
+        // variables
         float cont = 0;
         float media = 0;
+        float maximum = 0;
+
+
+        // sum of the numbers
+
         foreach (int number in numbers)
         {
             cont = cont + number;
         }
         Console.WriteLine($"The sum is: {cont}");
+
+        //average of the numbers
+
         media = cont / (numbers.Count - 1);
         Console.WriteLine($"The average is: {media}");
+
+        //maximum number
+        maximum = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > maximum)
+            {
+                maximum = number;
+            }
+        }       
+        Console.WriteLine($"The maximum is: {maximum}");
 
 
     }
