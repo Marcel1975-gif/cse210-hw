645a307cdb453c2eb5675635619dcc4e789864d4
Marcelo1975-gif <marcelocandidodasilva1975@gmail.com>
2025-09-08 18:26:04 -0300

    changed 05

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -8,20 +8,25 @@ class Program
         Job job1 = new Job();
         job1._company = "Microsoft";
         job1._jobTitle = "Software Engineer";
-        job1._startYear = 2020; 
+        job1._startYear = 2020;
         job1._endYear = 2023;
 
 
         Job job2 = new Job();
         job2._company = "Apple";
-        job2._jobTitle = "Software development"; 
+        job2._jobTitle = "Software development";
         job2._startYear = 2024;
         job2._endYear = 2025;
 
         job1.DisplayJobDetails();
         job2.DisplayJobDetails();
 
-
+        Resume myResume = new Resume();
+        myResume._name = "John Doe";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.DisplayResume();
+        
 
 
 
