78d2a603c157fa60f521c365b57a9c55ead35202
Marcelo1975-gif <marcelocandidodasilva1975@gmail.com>
2026-02-28 20:35:15 -0300

    Exercise 1 completed

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("WaitHandleExtensions is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine("Your favorite color is: " + color);
     }
 }
