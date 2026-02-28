ff56fa63062d992334fa58a86193aa3eeadc2ce0
Marcelo1975-gif <marcelocandidodasilva1975@gmail.com>
2026-02-28 20:40:54 -0300

    Practicing commit messages.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,7 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
-        Console.WriteLine("WaitHandleExtensions is your favorite color?");
+        Console.WriteLine("Practice makes perfect!");
+
+
+        Console.WriteLine("What is your favorite color?");
         string color = Console.ReadLine();
         Console.WriteLine("Your favorite color is: " + color);
     }
